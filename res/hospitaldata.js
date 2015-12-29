729117435516610d5c4423089685a4d3f865315e
penghaozhong <penghaozhong@163.com>
2015-12-29 15:04:14 +0800

    完成全部科室，医院下拉框功能

---
+++ res/hospitaldata.js
@@ -0,0 +1,83 @@
+/**
+ * Created by penghaozhong on 15/12/29.
+ */
+var diqu=new Array();
+var yiyuan=new Array();
+diqu[0]='0';
+diqu[diqu.length]='c38077';
+diqu[diqu.length]='c93468';
+diqu[diqu.length]='c43942';
+yiyuan[yiyuan.length]=new Array('cf13a0b96-0238-4f6c-94c8-5edbd751e29f','c38077');
+yiyuan[yiyuan.length]=new Array('ce703db2c-8641-4326-8edd-cbb70b592726','c38077');
+yiyuan[yiyuan.length]=new Array('c7406ba50-c6de-4541-bbaa-29473747603a','c38077');
+yiyuan[yiyuan.length]=new Array('c14ac6b70-86a5-472c-a72d-898c272e242d','c38077');
+yiyuan[yiyuan.length]=new Array('c38811e4b-1375-4667-a720-d8bafac9940e','c38077');
+yiyuan[yiyuan.length]=new Array('c6dd681a6-fac2-4cbf-a5db-5fc006a87333','c38077');
+yiyuan[yiyuan.length]=new Array('cb5d6c9b2-e6bf-403c-8c3f-0cf15b81a26e','c38077');
+yiyuan[yiyuan.length]=new Array('cefdaad1b-2e81-462f-ae9a-eb5ae93db9de','c38077');
+yiyuan[yiyuan.length]=new Array('c9a5bad1f-f8dd-46ae-a33b-c810e57bacb1','c38077');
+yiyuan[yiyuan.length]=new Array('c38281553-804e-4390-8022-a77a2913371f','c38077');
+yiyuan[yiyuan.length]=new Array('c1bc2600b-9547-4690-a598-a66ac484dd5c','c38077');
+yiyuan[yiyuan.length]=new Array('c4e78549a-807c-4978-a859-02278cd035e7','c38077');
+yiyuan[yiyuan.length]=new Array('c1fbe8842-a6d5-4733-8171-17899cebd6b7','c38077');
+yiyuan[yiyuan.length]=new Array('c8b870a68-1ee4-458d-a33f-9320cd419bed','c38077');
+yiyuan[yiyuan.length]=new Array('c8a41f671-74f3-4c30-8767-de5ff77a7e92','c38077');
+
+var diqu1=new Array();
+var yiyuan1=new Array();
+diqu1[0]='0';
+diqu1[diqu1.length]='c77071';
+diqu1[diqu1.length]='c67512';
+diqu1[diqu1.length]='c92091';
+diqu1[diqu1.length]='c69558';
+diqu1[diqu1.length]='c35420';
+diqu1[diqu1.length]='c21115';
+<!--口腔科(12)-->
+<!--影像医学与核医学-->
+/*yiyuan1[yiyuan1.length]=new Array('cb05a9646-7640-492b-921a-47e47e7fc460','');*/
+yiyuan1[yiyuan1.length]=new Array('cb4eaa217-a2ba-41dc-ba4b-c6e5f55d2718','c10582');
+yiyuan1[yiyuan1.length]=new Array('c6235d3f6-f302-480a-b73e-c1305d20b217','c65191');
+yiyuan1[yiyuan1.length]=new Array('cedc691a4-3c47-4f37-ae09-e812a0bb3389','c65191');
+yiyuan1[yiyuan1.length]=new Array('cc6f6e683-fc48-435b-a90a-c1aeabfa9bcc','c77071');
+yiyuan1[yiyuan1.length]=new Array('c5f5e65ab-398e-45b9-9573-5667ff2ab347','c17772');
+yiyuan1[yiyuan1.length]=new Array('cb0a814db-6767-41a5-ba96-d39a279c3d43','c10582');
+yiyuan1[yiyuan1.length]=new Array('cad5628a0-76bc-45bf-b79e-67a5b88293bc','c65191');
+yiyuan1[yiyuan1.length]=new Array('c0ab3934c-997c-4373-9a28-2002d316ce49','c65191');
+yiyuan1[yiyuan1.length]=new Array('c406761c4-3c32-4750-8a68-b4ff3c246e1f','c21115');
+yiyuan1[yiyuan1.length]=new Array('cb43d17d7-c459-46c6-87fe-7e7479c8d583','c65191');
+yiyuan1[yiyuan1.length]=new Array('c58d9f8e0-f2b1-452f-aa1d-3e99e5b72fd9','c77071');
+yiyuan1[yiyuan1.length]=new Array('c5eba9ec5-fe35-4eba-8482-f3c2e187cb03','c21115');
+yiyuan1[yiyuan1.length]=new Array('cca1a9f9a-535d-4a93-be25-b34a53fe14f8','c77071');
+yiyuan1[yiyuan1.length]=new Array('c1ecf4dbe-4703-4ca1-9474-7be0c4554baf','c21115');
+yiyuan1[yiyuan1.length]=new Array('caad92c64-2dc1-4873-9f8d-7af9274f5e47','c56303');
+yiyuan1[yiyuan1.length]=new Array('c4ce232b8-7235-4cb5-801f-82f55ec303a4','c65191');
+yiyuan1[yiyuan1.length]=new Array('c7e94ad2e-fd5b-4c91-ba57-bf7bff099c65','c17772');
+yiyuan1[yiyuan1.length]=new Array('c1971e5f2-d54d-42f6-a249-96efdb9f37b6','c67512');
+yiyuan1[yiyuan1.length]=new Array('cac36ff4b-7b01-42c2-9c7d-78b77c4862cd','c10582');
+yiyuan1[yiyuan1.length]=new Array('c6c141ae8-6fb5-4da0-b31e-66b2b2d2d82e','c35420');
+yiyuan1[yiyuan1.length]=new Array('ccac0931c-f3e3-4390-a5cf-62656e065038','c67512');
+yiyuan1[yiyuan1.length]=new Array('ca96d5113-efd7-4401-9574-6152c31347a5','c10582');
+yiyuan1[yiyuan1.length]=new Array('c596a397d-d598-4237-9ed5-140cd8d624cc','c56303');
+yiyuan1[yiyuan1.length]=new Array('c80dc35f0-deb9-44df-9f06-e649d15f854d','c77071');
+yiyuan1[yiyuan1.length]=new Array('cea110805-2ece-4da7-985d-4dc60a519d54','c21115');
+yiyuan1[yiyuan1.length]=new Array('c3e61627a-6b3c-47b9-bfad-95f8f189941b','c77071');
+yiyuan1[yiyuan1.length]=new Array('cbb73437e-499e-48a3-8f02-0753f40437e2','c10582');
+yiyuan1[yiyuan1.length]=new Array('c916a9a75-8ffd-4ff3-bba8-833d89f16085','c21115');
+yiyuan1[yiyuan1.length]=new Array('c0c8ea831-8c6b-496e-8b87-85efd9ba8d24','c77071');
+yiyuan1[yiyuan1.length]=new Array('cd0942bb4-2ab4-4e8a-942b-b6a4a464a4da','c10582');
+yiyuan1[yiyuan1.length]=new Array('cf19e3d3b-f5f2-4d96-b14e-5662efea6d5e','c21115');
+yiyuan1[yiyuan1.length]=new Array('c83fff522-11f0-45d3-9cd1-aea5ce184309','c77071');
+yiyuan1[yiyuan1.length]=new Array('ccdd7052f-ec3a-4df5-845c-5655bf8044ec','c67512');
+yiyuan1[yiyuan1.length]=new Array('c5ee822f4-c24f-4bbd-846f-417ca8888fb1','c67512');
+yiyuan1[yiyuan1.length]=new Array('ca524d110-eeb3-4534-a573-13a0a417d7db','c17772');
+yiyuan1[yiyuan1.length]=new Array('c23502116-ef68-4a21-9580-3d6eccdea16c','c77071');
+yiyuan1[yiyuan1.length]=new Array('cf9f954e3-f56f-4a13-b598-d8958dcdabcc','c69558');
+yiyuan1[yiyuan1.length]=new Array('c9c07fc0d-d4a9-429a-bda8-409666c1477d','c10582');
+yiyuan1[yiyuan1.length]=new Array('cfec49cc9-9771-4297-b9f8-5f5c3aa2f10f','c67512');
+yiyuan1[yiyuan1.length]=new Array('c633e4dd8-5cd2-4087-af8c-3519cb93b095','c67512');
+yiyuan1[yiyuan1.length]=new Array('cb348cb9e-b25f-4b79-b556-a8838a831d9c','c17772');
+yiyuan1[yiyuan1.length]=new Array('c373a2bfb-f721-44a7-a23d-092c850e28c0','c69558');
+yiyuan1[yiyuan1.length]=new Array('c50fe2625-9844-4f66-818f-1de7834dbad0','c21115');
+yiyuan1[yiyuan1.length]=new Array('c64094c96-92da-47da-8b93-691abe22d44c','c67512');
+
+/*中西医结研究所（1）*/
